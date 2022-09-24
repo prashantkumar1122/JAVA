c699da27561ef245f3879c3f95b575ce7f3c00f5
Prashant kumar <104028213+prashantkumar1122@users.noreply.github.com>
2022-09-25 03:00:55 +0530

    Create User_Input_Value_Square.java

---
+++ Some_Basic_Input_Programs/User_Input_Value_Square.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class User_Input_Value_Square 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Here we are Squaring the number by user input");
+        Scanner sc = new Scanner(System.in);
+      
+        System.out.println("Enter the number :");
+        int a = sc.nextInt();
+        int result = a*a;
+      
+        System.out.println("The Square of this number is :"+ result);
+    }
+}
